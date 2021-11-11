23ac5393400d291eb454d222c5d6cbb606a196c4
indonswe <afc2002afc@hotmail.com>
2021-11-11 09:51:55 +0100

    first commit

---
+++ src/components/DataTable.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DataTable = () => {
+    return (
+        <div>
+            Yo!
+        </div>
+    );
+};
+
+export default DataTable;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import ReactDOM from "react-dom";
+// import bootstrap css
+import "bootstrap/dist/css/bootstrap.css";
+import DataTable from "./components/DataTable";
+
+ReactDOM.render(<DataTable />, document.getElementById("root"));
