442d03ffd3b4c8dde1c5f7baa1cd8dcb16f2ceb3
indonswe <afc2002afc@hotmail.com>
2021-11-14 22:37:41 +0100

    Route or react

---
+++ src/components/DemoRouter.js
@@ -0,0 +1,59 @@
+//import React from 'react';
+import { BrowserRouter, Route, Routes, NavLink } from 'react-router-dom'
+
+
+const DemoRouter = () => {
+
+const Welcome = () => {
+    return (
+        <div>
+          <h1>Welcome</h1>
+          
+    </div>
+      );
+        };
+
+
+const Person = () => {
+    return (
+        <div>
+          <h1>Person</h1>
+        </div>
+      );
+        };
+
+        const Menunav = () => {
+          return (
+              <div>
+                <BrowserRouter>
+        <nav>
+          <h1>My Articles</h1>
+          <NavLink exact to="/">Welcome</NavLink>
+          <NavLink to="/Person">About</NavLink>
+          
+        </nav>
+
+        <Routes>
+          <Route exact path="/">
+            <Welcome />  
+          </Route>
+          <Route path="/Person">
+            <Person />  
+          </Route>
+          
+        </Routes>
+
+      </BrowserRouter>
+                </div>
+            );
+              };
+
+
+    return (
+        <div>
+            <Menunav/>
+        </div>
+    );
+};
+
+export default DemoRouter;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import ReactDOM from "react-dom";
 // import bootstrap css
 import "bootstrap/dist/css/bootstrap.css"; 
-import DataTable from "./components/DataTable";
+//import DataTable from "./components/DataTable";
+import DemoRouter from "./components/DemoRouter";
 
-ReactDOM.render(<DataTable />, document.getElementById("root"));
+ReactDOM.render(<DemoRouter />, document.getElementById("root"));
+//ReactDOM.render(<DataTable />, document.getElementById("root"));
